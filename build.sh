985e1de62679cf0a6451047bca6bcb8e5af78dad
Jim Park <jpark@jim80.net>
2018-12-28 13:18:54 -0800

    cloudbuild output generation
    results go to: https://storage.googleapis.com/jim-resume/

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#hackmyresume build resume.json TO out/resume.all -t themes/node_modules/jsonresume-theme-flat/
-
-docker run --rm -v $(pwd):/resume tmjd/hackmyresume hackmyresume BUILD -t node_modules/jsonresume-theme-flat/ /resume/resume.json TO /resume/out/index.all
-
