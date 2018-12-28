ce30fc6b3bb897a4ed2411371511a2218f33c967
Jim Park <jpark@jim80.net>
2018-12-28 11:51:41 -0800

    doit with a script first

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-hackmyresume build resume.json TO out/resume.all -t themes/node_modules/jsonresume-theme-flat/
+#hackmyresume build resume.json TO out/resume.all -t themes/node_modules/jsonresume-theme-flat/
+
+docker run --rm -v $(pwd):/resume tmjd/hackmyresume hackmyresume BUILD -t node_modules/jsonresume-theme-flat/ /resume/resume.json TO /resume/out/index.all
+
